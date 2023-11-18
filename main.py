15ea44bbd0da7b9eb72ac40cefb1a0b028effc9a
NayakantiManojkumar <106529428+NayakantiManojkumar@users.noreply.github.com>
2023-11-18 21:38:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ parser.add_option("--website", dest="website",help="choose a website")
 (options, args) = parser.parse_args()
 
 
-CHROME_DVR_DIR = 'C:\webdrivers\chromedriver.exe'
+CHROME_DVR_DIR = "C:\Users\N Manojkumar\Downloads\chrome-win64"
 
 def wizard():
     print (banner)
